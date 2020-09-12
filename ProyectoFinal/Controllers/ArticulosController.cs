647d1a249c86431256cc228b0150babd6378f25c
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2020-09-11 21:07:39 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Controllers/ArticulosController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Mantenimiento.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        private DBRestauranteEntities db = new DBRestauranteEntities();
+
+        // GET: Articulos
+        public ActionResult Index()
+        {
+            return View(db.Articuloes.ToList());
+        }
+
+        // GET: Articulos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ArticuloID,Nombre,Descripcion,Precio,Estado,Imagen")] Articulo articulo, HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                    string ImageName = System.IO.Path.GetFileName(file.FileName);
+                    string physicalPath = Server.MapPath("~/Images/" + ImageName);
+                    file.SaveAs(physicalPath);
+
+                    if (ModelState.IsValid)
+                        {
+                             articulo.Imagen = ImageName;
+                            db.Articuloes.Add(articulo);
+                            db.SaveChanges();
+                            return RedirectToAction("Index");
+                        }
+            }
+                return View(articulo);
+        }
+
+        // GET: Articulos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ArticuloID,Nombre,Descripcion,Precio,Estado")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Articulo articulo = db.Articuloes.Find(id);
+            db.Articuloes.Remove(articulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Mantenimiento.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private DBRestauranteEntities db = new DBRestauranteEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaArticulos()
+        {
+            return View(db.Articuloes.ToList().Where(x => x.Estado == true));
+        }
+
+        public ActionResult RentarArticulo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulos/
+        [HttpPost, ActionName("RentarArticulo")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmarRenta(int id)
+        {
+            Articulo articulo = db.Articuloes.Find(id);
+            articulo.Estado = false;
+            db.Entry(articulo).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("ListaArticulos");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
